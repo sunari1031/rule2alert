52b376ab7583ed03d5ea7400ad3ec91f9af06968
pablo.rincon.crespo <pablo.rincon.crespo@dabe24e0-e1cc-11de-9fd5-4be1c9d5bc18>
2009-12-13 01:41:57 +0000

    Adding some print functions for inspecting payloads

--- Generator/Payload.py
+++ Generator/Payload.py
@@ -1,12 +1,19 @@
 import struct
 from binascii import *
 from ctypes import create_string_buffer
+import sys
+import string
 
 class PayloadGenerator:
+    pkts = []
     payload = None
     contents = []
+    itered = []
+
     def __init__(self, rule_contents):
         self.contents = rule_contents
+        self.payload = None
+        self.itered = []
         
     def build(self):
         oldc = None
@@ -46,8 +53,18 @@ class PayloadGenerator:
             itered.append(c)
             print "-> Ini: " + str(c.ini) + " End: " + str(c.end)
 
-        # perform padding
-        self.payload = create_string_buffer(c.end)
+        # buffer size
+        max = 0
+        for c in itered:
+            if c.end > max:
+                max = c.end
+
+        # perform padding with ' 's (blank spaces)
+        padding = ""
+        for i in range(0,max):
+            padding = padding + " "
+        self.payload = create_string_buffer(max)
+        struct.pack_into(str(max) + "s", self.payload, 0, padding)
 
         # write payloads
         for c in itered:
@@ -63,7 +80,6 @@ class PayloadGenerator:
                         continue
 
                     if c.content[i]=="|" and i>0 and c.content[i-1]!='\\' and flag == 1:
-                        print "unsetting"
                         flag = 0
                         i = i + 1
                         continue
@@ -73,17 +89,44 @@ class PayloadGenerator:
                         continue
 
                     if flag == 1:
-                        tmp = tmp + "\\x%s" % c.content[i:i+2]
-                        print "\\x" + c.content[i:i+2]
+                        tmp = tmp + a2b_hex(c.content[i:i+2])
                         i = i + 1
                     else:
                         tmp = tmp + c.content[i]
                         
                     i = i + 1
 
-                print "..."+tmp +" " + str(len(tmp))
                 struct.pack_into(fmt, self.payload, c.ini, tmp)
 
+        self.itered = itered
         return self.payload
+
+    def hexPrint(self):
+        print "-------- Hex Payload Start ----------"
+        for i in range(0,len(self.payload)):
+            sys.stdout.write(" " + hexlify(self.payload[i]))
+            if i > 0 and (i + 1) % 4 == 0:
+                sys.stdout.write(" ")
+            if i > 0 and (i + 1) % 8 == 0:
+                print ""
+        print "--------- Hex Payload End -----------"
+
+    def asciiPrint(self):
+        print "-------- Ascii Payload Start ----------"
+        print self.payload.raw
+        for i in range(0,len(self.payload)):
+            c = self.payload.raw[i]
+            if c in string.printable:
+                sys.stdout.write(c)
+            else:
+                sys.stdout.write("\\x" + hexlify(c))
+        print "\n--------- Ascii Payload End -----------"
             
-# We should deal here with the http modifiers (skiping by now)
+    def PrintOffsets(self):
+        print " Start        End"        
+        if self.itered == []:
+            return
+        for c in self.itered:
+            print "%05s  %10s" % (str(c.ini), str(c.end))
+# We should deal with the http modifiers (skiping by now)
+#
