da093f2c0fd06cb668db1a518ad7db6ae225255d
famousjs <famousjs@dabe24e0-e1cc-11de-9fd5-4be1c9d5bc18>
2010-01-25 23:09:00 +0000

    Fixing

--- trunk/Parser/SnortConf.py
+++ trunk/Parser/SnortConf.py
@@ -20,11 +20,26 @@ class SnortConf:
 				var,data = line[4:].split(" ")
 				if data[1:] in self.vars:
 					data = self.vars[data[1:]]
+				if data.startswith("!"):
+					tmp = self.vars[data[2:]]
+					if tmp.startswith("192."):
+						data = "10.0.0.1"
+					elif tmp.startswith("10."):
+						data = "192.168.0.1"
+					#data = self.vars[data[2:]]
 				self.vars[var] = data
 			elif line.startswith("portvar"):
-				var, data = line[8:].split(" ")
+				var, data = line[8:].strip().split(" ")
 				if data[1:] in self.vars:
 					data = self.vars[data[1:]]
+				if data.startswith("!"):
+					data = int(data[1:]) + 1
+					#tmp = self.vars[data[2:]]
+					#if tmp.startswith("192."):
+					#	data = "10.0.0.1"
+					#elif tmp.startswith("10."):
+					#	data = "192.168.0.1"
+					#data = self.vars[data[2:]]
 				self.vars[var] = data
 
 	
