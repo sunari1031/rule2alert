a1ee68a0c5e57669fc5204673453203eb0580e8e
famousjs <famousjs@dabe24e0-e1cc-11de-9fd5-4be1c9d5bc18>
2010-01-26 00:08:43 +0000

    Trying to fix

--- Parser/SnortConf.py
+++ Parser/SnortConf.py
@@ -20,26 +20,18 @@ class SnortConf:
 				var,data = line[4:].split(" ")
 				if data[1:] in self.vars:
 					data = self.vars[data[1:]]
-				if data.startswith("!"):
-					tmp = self.vars[data[2:]]
-					if tmp.startswith("192."):
-						data = "10.0.0.1"
-					elif tmp.startswith("10."):
-						data = "192.168.0.1"
-					#data = self.vars[data[2:]]
 				self.vars[var] = data
 			elif line.startswith("portvar"):
 				var, data = line[8:].strip().split(" ")
 				if data[1:] in self.vars:
 					data = self.vars[data[1:]]
-				if data.startswith("!"):
+				if data.startswith("!$"):
+					data = self.vars[data[2:]]
+				elif data.startswith("!"):
 					data = int(data[1:]) + 1
-					#tmp = self.vars[data[2:]]
-					#if tmp.startswith("192."):
-					#	data = "10.0.0.1"
-					#elif tmp.startswith("10."):
-					#	data = "192.168.0.1"
-					#data = self.vars[data[2:]]
+				elif data.startswith("$"):
+					data = self.vars[data[1:]]
+
 				self.vars[var] = data
 
 	
