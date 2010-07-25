30c8b6a8d4e585d6c5fe7bb9610fc5cf6153e5e5
Famousjs <Famousjs@dabe24e0-e1cc-11de-9fd5-4be1c9d5bc18>
2010-07-25 22:51:31 +0000

    Testing new revregex lib

--- Generator/Payload.py
+++ Generator/Payload.py
@@ -1,12 +1,8 @@
-import struct
+import struct,sys,string,traceback,random
 from binascii import *
 from ctypes import create_string_buffer
 from scapy.all import *
 from Protocols.HTTP import *
-import sys
-import string
-import traceback
-import random
 
 class PayloadGenerator:
 	pkts = []
@@ -57,24 +53,21 @@ class PayloadGenerator:
 		elif self.rule.rawdestinations[1:] == "HOME_NET":
 			self.home = "dst"
 	
-		#if self.rule.rawsources[1:] == "EXTERNAL_NET":
-		#	self.ext = "src"
-		#elif self.rule.rawdestinations[1:] == "EXTERNAL_NET":
-		#	self.ext = "dst"
-		
-
 		self.sport	   = ""
 		self.dport	   = ""
 		self.proto	   = self.rule.proto
-		self.protocol  = ""
+		self.protocol      = ""
 		self.ip  	   = IP()
-		self.handshake = False
-		self.packets   = []
+		self.handshake     = False
+		self.packets       = []
 
 		self.parseComm(self.rule.rawsrcports, self.rule.rawdesports)
 		#self.build()
-		
+
 	def build(self):
+		#Simple HTTP string based checks
+		httpCheck = ["POST","GET","User-Agent","Host","Cookie"]
+
 		if self.flow and self.flow.established:
 			self.build_handshake()
 
@@ -148,22 +141,21 @@ class PayloadGenerator:
 		#ADDED - FOR HTTP SUPPORT
 		h = None
 		if self.sport == "80" or self.dport == "80":
-			if self.payload.raw.find("User-Agent") != -1 or self.payload.raw.find("GET") != -1 or self.payload.raw.find("POST") != -1:
-				h = HTTP()
-				h.check(self.payload.raw)
-				h.build()
+			for check in httpCheck:
+				if self.payload.raw.lower().find(check.lower()) != -1:
+					h = HTTP()
+					h.check(self.payload.raw)
+					h.build()
+					break
 			if self.uricontents:
 				if not h: h = HTTP()
 				uri = ""
 				for u in self.uricontents:
-					#print u.uricontent
 					uri = "%s%s" % (uri, str(u.uricontent))
 				h.uri = uri
 				h.build()
 		if h:
 			if self.payload.raw:
-			#	h.method = "POST"
-			#	h.build()
 				h.payload = h.payload + self.payload.raw
 			self.build_packet(h.payload)
 			return h.payload
@@ -177,22 +169,6 @@ class PayloadGenerator:
 		dest_ip	    = self.ip.dst
 		dest_port   = self.protocol.dport
 		flag        = "PA"
-
-		#Set flow
-		#if self.flow is not None:
-		#	if self.flow.to_client or self.flow.from_server:
-		#		source_ip = self.ip.dst
-		#		source_port = self.protocol.dport
-		#		dest_ip = self.ip.src
-		#		dest_port = self.protocol.sport
-		#		flag = "A"
-		#	elif self.flow.stateless:
-		#		flag = "A"
-		#if self.flip:
-		#	source_ip = self.ip.dst
-		#	source_port = self.protocol.dport
-		#	dest_ip = self.ip.src
-		#	dest_port = self.protocol.sport
 
 		if self.proto == "tcp":
 			seq_num, ack_num = self.get_seqack()
@@ -234,13 +210,6 @@ class PayloadGenerator:
 	
 			self.packets.append(p)
 
-
-
-		#self.packets.append(p)
-		#self.packets.append(fin_ack)
-		#self.packets.append(ack)
-
-
 	def build_handshake(self):
 		ipsrc   = self.ip.src
 		ipdst   = self.ip.dst
@@ -279,14 +248,6 @@ class PayloadGenerator:
 			seq = self.packets[-1].seq
 			ack = self.packets[-1].ack
 
-		#if self.flow is not None:
-		#	if self.flow.to_client or self.flow.from_server:
-		#		#QUICK FIX
-		#		#seq = self.packets[-2].seq + 1
-		#		seq = self.packets[-1].seq
-		#		ack = self.packets[-1].ack
-				#ack = self.packets[-1].seq
-			
 		return seq,ack
 
 	def parseComm(self, sports, dports):
@@ -298,6 +259,45 @@ class PayloadGenerator:
 			print "Protocol Not Supported"
 			self.notSupported = True
 			return 1
+
+		self.src = re.sub("\$",'',self.src)
+		self.dst = re.sub("\$",'',self.dst)
+		sports   = re.sub("\$",'',sports)
+		dports   = re.sub("\$",'',dports)
+
+		for var in self.snort_vars:
+			m = re.search(var, self.src)
+			if m:
+				self.src = re.sub(var, self.snort_vars[var], self.src)
+				break
+
+		for var in self.snort_vars:
+			m = re.search(var, self.dst)
+			if m:
+				self.dst = re.sub(var, self.snort_vars[var], self.dst)
+				break
+
+		if self.src.find("[") != -1:
+			self.src = self.src.split(",")[0]
+			self.src = re.sub("\[",'',self.src)
+			self.src = re.sub("\]",'',self.src)
+		
+		if self.dst.find("[") != -1:
+			self.dst = self.dst.split(",")[0]
+			self.dst = re.sub("\[",'',self.dst)
+			self.dst = re.sub("\]",'',self.dst)
+
+		if self.src.find("!") != -1:
+			if self.src.find(self.snort_vars["HOME_NET"]) != -1:
+				self.src = self.snort_vars["EXTERNAL_NET"]
+			elif self.src.find(self.snort_vars["EXTERNAL_NET"]) != -1:
+				self.src = self.snort_vars["HOME_NET"]
+
+		if self.dst.find("!") != -1:
+			if self.dst.find(self.snort_vars["HOME_NET"]) != -1:
+				self.dst = self.snort_vars["EXTERNAL_NET"]
+			elif self.dst.find(self.snort_vars["EXTERNAL_NET"]) != -1:
+				self.dst = self.snort_vars["HOME_NET"]
 		#If the source is using CIDR notiation
 		#Just pick the first IP in the subnet
 		if self.src.find("/") != -1:
@@ -321,11 +321,20 @@ class PayloadGenerator:
 			print self.src
 			print self.dst
 
-		#Do the same type of thing for ports
-		if sports[1:] in self.snort_vars:
-			self.sport = str(self.snort_vars[sports[1:]])
-		elif sports == "any":
-			#self.sport = "9001"
+
+		for var in self.snort_vars:
+			m = re.search(var, sports)
+			if m:
+				sports = re.sub(var, self.snort_vars[var], sports)
+				break
+
+		for var in self.snort_vars:
+			m = re.search(var, dports)
+			if m:
+				dports = re.sub(var, self.snort_vars[var], dports)
+				break
+
+		if sports == "any":
 			self.sport = str(RandShort())
 		else:
 			self.sport = str(sports)
@@ -336,11 +345,10 @@ class PayloadGenerator:
 			self.sport = str(int(self.sport[1:]) -1)
 		if self.sport.find("[") != -1:
 			self.sport = str(self.sport.split(",")[1])
+			self.sport = re.sub("\[",'',self.sport)
+			self.sport = re.sub("\]",'',self.sport)
 
-		if dports[1:] in self.snort_vars:
-			self.dport = str(self.snort_vars[dports[1:]])
-		elif dports == "any":
-			#self.dport = "9001"
+		if dports == "any":
 			self.dport = str(RandShort())
 		else:
 			self.dport = str(dports)
@@ -351,6 +359,8 @@ class PayloadGenerator:
 			self.dport = str(int(self.dport[1:]) -1)
 		if self.dport.find("[") != -1:
 			self.dport = str(self.dport.split(",")[1])
+			self.dport = re.sub("\[",'', self.dport)
+			self.dport = re.sub("\]",'',self.dport)
 
 		try:
 			self.protocol.sport = int(self.sport)
